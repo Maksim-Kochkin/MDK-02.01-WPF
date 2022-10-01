e21bec4e1623944a7483765670a5a47614eb0059
Maksim-Kochkin <106807060+Maksim-Kochkin@users.noreply.github.com>
2022-10-01 22:06:28 +0500

    Задание 1 и 2 для 2й подгруппы

---
+++ Упражнение №1 (пример окна)/Упражнение №1 (пример окна)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Упражнение__1__пример_окна_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Упражнение №2/Упражнение №2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Упражнение__2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " отмечен");
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " не отмечен");
+        }
+
+        private void checkBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(checkBox.Content.ToString() + " в неопределенном состоянии");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
