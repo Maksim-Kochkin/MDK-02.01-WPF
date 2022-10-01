ec52f4cde7356163fd32d8fb29e47ae4444a33c2
Maksim-Kochkin <106807060+Maksim-Kochkin@users.noreply.github.com>
2022-10-01 22:09:20 +0500

    Упражнение 3 для 2й подгруппы, часть "с"

---
+++ Задача №1 (В)/Задача №1 (В)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Задача__1__В_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Задача №1 (В)/Задача №1 (В)/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задача__1__В_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Type(); Size(); Topings();
+            Result();
+        }
+
+        private void ResultTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        public void Type()
+        {
+
+        }
+        public void Size()
+        {
+
+        }
+        public void Topings()
+        {
+
+        }
+        public void Result()
+        {
+            int Result = 0; string Res = Convert.ToString(Result);
+            ResultTb.Text = Res;
+        }
+    }
+}
