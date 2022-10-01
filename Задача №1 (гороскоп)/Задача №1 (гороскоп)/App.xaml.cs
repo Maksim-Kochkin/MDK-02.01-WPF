570ae169f86bb86b602548c0f19fa99921b2d79f
Maksim-Kochkin <106807060+Maksim-Kochkin@users.noreply.github.com>
2022-10-01 22:10:30 +0500

    Задания за 3ю пару для всей группы

---
+++ Задача №1 (гороскоп)/Задача №1 (гороскоп)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Задача__1__гороскоп_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
